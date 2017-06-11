9e71825e34f3222b9202f75cea3dc7e24afb27c0
Mihir Takalkar <papyarocks@gmail.com>
2017-06-11 12:28:34 -0700

    Made Snake Class.

---
+++ MihirSnake/MihirSnake/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MihirSnake
+{
+    public partial class Form1 : Form
+    {
+
+        //Snake (represents 1 peice of the snake)
+        //Food
+
+            //get a square on the screen and move it around 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MihirSnake/MihirSnake/Snake.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MihirSnake
+{
+    class Snake
+    {
+        public int squarex;
+        public int squarey;
+        public int height;
+        public int width;
+        public int speed;
+
+        enum Direction
+        {
+            Stopped,
+            Up,
+            Down,
+            Left,
+            Right
+        }
+
+        Direction direction;
+
+        public Snake(int squarex, int squarey, int height, int width, int speed)
+        {
+            this.squarex = squarex;
+            this.squarey = squarey;
+            this.height = height;
+            this.width = width;
+            this.speed = speed;
+        }
+    }
+}
